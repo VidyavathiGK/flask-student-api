5ec2963499438d440e1cc2a88ccda4b17bd38475
Vidyavathi GK <vidyavidyavathigk@gmail.com>
2026-02-17 22:44:02 +0530

    Create app.py
    Added flask student API

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+students = {}
+
+@app.route("/")
+def home():
+    return "Welcome to Flask Student API 🚀"
+
+@app.route("/students", methods=["POST"])
+def add_student():
+    data = request.get_json()
+
+    student_id = data["id"]
+    students[student_id] = {
+        "name": data["name"],
+        "age": data["age"],
+        "course": data["course"]
+    }
+
+    return jsonify({"message": "Student added successfully"}), 201
+
+
+@app.route("/students", methods=["GET"])
+def get_students():
+    return jsonify(students)
+
+
+@app.route("/students/<student_id>", methods=["PUT"])
+def update_student(student_id):
+    if student_id in students:
+        data = request.get_json()
+        students[student_id].update(data)
+        return jsonify({"message": "Student updated successfully"})
+    return jsonify({"error": "Student not found"}), 404
+
+
+@app.route("/students/<student_id>", methods=["DELETE"])
+def delete_student(student_id):
+    if student_id in students:
+        del students[student_id]
+        return jsonify({"message": "Student deleted successfully"})
+    return jsonify({"error": "Student not found"}), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
